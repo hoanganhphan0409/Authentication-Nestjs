01a5346eaa72a72f90e614b506b62e4ba1ae9942
hoanganhphan0409 <89737507+hoanganhphan0409@users.noreply.github.com>
2024-08-20 03:00:54 +0700

    add type orm

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,7 +1,9 @@
 import { Module } from "@nestjs/common";
 import { UserService } from "./user.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { User } from "src/typeorm/entity/User";
 @Module({
-    imports:[],
+    imports:[TypeOrmModule.forFeature([User])],
     controllers:[],
     providers:[UserService],
     exports:[UserService]
