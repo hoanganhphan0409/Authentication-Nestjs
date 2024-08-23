ed1f2dcf520782afef0f98ab7f967097694ed567
anhphgameestudio <89737507+hoanganhphan0409@users.noreply.github.com>
2024-08-23 14:19:48 +0700

    Update user.service.ts

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -6,19 +6,40 @@ import { Repository } from "typeorm";
 import { UserUpdateDto } from "./dto/userUpdate.dto";
 
 @Injectable()
-export class UserService{
+export class UserService {
    constructor(@InjectRepository(User)
    private userRepo: Repository<User>
-   ){}
-   async registerUser(newUser : UserDto) : Promise<UserDto>{
-      const user = this.userRepo.create(newUser);
-      return this.userRepo.save(user);
+   ) { }
+   async registerUser(newUser: UserDto): Promise<UserDto> {
+      try {
+         const user = this.userRepo.create(newUser);
+         return await this.userRepo.save(user);
+      } catch (error) {
+         throw new Error('Could not register user');
+      }
    }
-   async findUserByName(userName : string): Promise<User>{
-      return this.userRepo.findOne({ where: {name: userName } });      
+   async findUserByName(userName: string): Promise<User> {
+      try {
+         const user = await this.userRepo.findOne({ where: { name: userName } });
+         if (!user) {
+            throw new Error('User not found');
+         }
+         return user;
+      } catch (error) {
+         throw new Error('Could not find user');
+      }
    }
-   async updateUser(user : UserUpdateDto){
-      const filteredData = Object.fromEntries(Object.entries(user).filter((_,value)=> value !== undefined));
-      return await this.userRepo.update({name : user.name}, filteredData);
+
+   async updateUser(user: UserUpdateDto): Promise<void> {
+      try {
+         const filteredData = Object.fromEntries(Object.entries(user).filter(([_, value]) => value !== undefined));
+         const result = await this.userRepo.update({ name: user.name }, filteredData);
+         if (result.affected === 0) {
+            throw new Error('User update failed');
+         }
+      } catch (error) {
+         throw new Error('Could not update user');
+      }
    }
+
 }
